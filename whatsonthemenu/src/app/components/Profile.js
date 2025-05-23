6ef53e6ff6f064afb1109b3c6ca92f837d75cdf5
Felix334 <felixmayer02@gmail.com>
2025-05-23 20:32:30 +0200

    Update Profile.js

--- whatsonthemenu/src/app/components/Profile.js
+++ whatsonthemenu/src/app/components/Profile.js
@@ -1,15 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useRouter } from "next/router";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
+const Profile = () => {
 
-const Profile = () =>{
-    return(
+  return (
     <div>
-        <Avatar>
-            <AvatarImage src="./Image/account_profile_user_avatar_icon_219236.webp"/>
-            <AvatarFallback>Profil</AvatarFallback>
-        </Avatar>
+      <Avatar className="w-12 h-12 z-50">
+        <AvatarImage src="/Image/account_profile_user_avatar_icon_219236.webp" /> {/* Corrected src path */}
+        <AvatarFallback>Profil</AvatarFallback>
+        <Link href="./Routes/Profil"></Link>
+      </Avatar>
     </div>
-    )
-}
-export default Profile
+  );
+};
+
+export default Profile;
