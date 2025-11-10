f62b6618e05d00e6ce315e1224af9263fe6b8dc3
Felix334 <felixmayer02@gmail.com>
2025-11-10 15:45:10 +0100

    email

---
+++ whatsonthemenu/src/app/api/emailtest/route.ts
@@ -0,0 +1,26 @@
+import { sendEmail } from "../../../lib/nodemailer"
+import { NextRequest } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  try {
+    await sendEmail('felixmayer02@gmx.de', 'Test Subject', '<p>Hello from Nodemailer!</p>');
+    return new Response(JSON.stringify({ message: 'Email sent' }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error);
+      return new Response(JSON.stringify({ error: error.message }), {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    } else {
+      return new Response(JSON.stringify({ error: 'Unknown error occurred' }), {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
+  }
+}
+// Code funktioniert aber Email-Provider ist noch nicht vorhanden
---
+++ whatsonthemenu/src/lib/nodemailer.ts
@@ -0,0 +1,25 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST, // e.g., 'smtp.gmail.com'
+  port: parseInt(process.env.SMTP_PORT || '587'),
+  secure: process.env.SMTP_SECURE === 'true', // false for 587, true for 465
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+export const sendEmail = async (to: string, subject: string, html: string) => {
+  try {
+    await transporter.sendMail({
+      from: process.env.SMTP_FROM,
+      to,
+      subject,
+      html,
+    });
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error;
+  }
+};
