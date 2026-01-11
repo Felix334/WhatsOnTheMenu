7ea370055fabcac341fb7f5d3ee5a8cf57f33d1b
Felix334 <felixmayer02@gmail.com>
2026-01-11 20:18:01 +0100

    Psuh

--- whatsonthemenu/src/logger.js
+++ whatsonthemenu/src/logger.js
@@ -2,6 +2,7 @@ export function setup_logger() {
   // Simple logger setup, can be expanded
   return {
     info: (msg) => console.log(`INFO: ${msg}`),
-    error: (msg) => console.error(`ERROR: ${msg}`)
+    error: (msg) => console.error(`ERROR: ${msg}`),
+    hmrRefreshes: (msg) => console.log(`Server-Component-Refresh: ${msg}`)
   };
 }
