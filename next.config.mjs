bca1f76524bc5316b08b5358848c808f2f2c4123
Felix334 <felixmayer02@gmail.com>
2026-06-10 20:41:42 +0200

    remove old env

--- next.config.mjs
+++ next.config.mjs
@@ -36,11 +36,6 @@ const nextConfig = {
   reactStrictMode: true,
   outputFileTracingRoot: process.cwd(),
 
-  env: {
-    API_KEY: process.env.API_KEY,
-    ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
-  },
-
   async headers() {
     return [
       {
--- src/app/api/Admin/getData/getFinazes/route.js
+++
@@ -1,4 +0,0 @@
-import { prisma } from "@/lib/prisma";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
-import { NextResponse } from "next/server";
